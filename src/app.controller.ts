451af8ae3e9e52b16ed66ded181954443af264ad
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2024-02-07 14:19:41 +0700

    feat: add common check :money_with_wings:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './apis/modules/auth/decorators/public.decorator';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Default')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
+  @HttpCode(200)
+  @ApiOperation({
+    summary: 'common check',
+    description: 'common check',
+  })
   getHello(): string {
     return this.appService.getHandler();
   }
